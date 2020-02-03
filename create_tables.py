4e595a2d2894ef19bd928873a5f5913c21c68378
Bruno Rocha <bruno.sousax@gmail.com>
2020-02-02 23:08:45 -0300

    Docstring for CREATE_TABLE script

--- create_tables.py
+++ create_tables.py
@@ -1,8 +1,22 @@
+"""This is the create_tables module.
+This module is responsible for create the database sparkifydb on postgres,
+also will create all tables that is described on sql_queries.py module.
+"""
+
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 
 def create_database():
+    """This function will create the database sparkifydb on postgres.
+    
+    Args:
+        None
+        
+    Returns:
+        None
+    """
+    
     # connect to default database
     conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
     conn.set_session(autocommit=True)
@@ -23,18 +37,48 @@ def create_database():
 
 
 def drop_tables(cur, conn):
+    """This function will drop all tables that is described on sql_queries.py module
+    
+    Args:
+        cur (psycopg2.extensions.cursor): The cursor from your connection with postgres
+        conn (psycopg2.extensions.connection): The connection with postgres
+        
+    Returns:
+        None
+    """
+    
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """This function will create all tables that is described on sql_queries.py module
+    
+    Args:
+        cur (psycopg2.extensions.cursor): The cursor from your connection with postgres
+        conn (psycopg2.extensions.connection): The connection with postgres
+        
+    Returns:
+        None
+    """
+    
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """This is the main function, will start with this module
+    and execute all another functions.
+    
+    Args:
+        None
+        
+    Returns:
+        None
+    """
+    
     cur, conn = create_database()
     
     drop_tables(cur, conn)
