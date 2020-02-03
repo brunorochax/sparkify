b21ac2ea0af7a6414a3f2e198a3634fce2a565fd
Bruno Rocha <bruno.sousax@gmail.com>
2020-02-02 21:32:39 -0300

    Including pk and fk on tables

--- sql_queries.py
+++ sql_queries.py
@@ -8,19 +8,20 @@ time_table_drop = "drop table if exists time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""create table if not exists songplays(songplay_id serial, start_time timestamp, user_id varchar, \
-level varchar, song_id varchar, artist_id varchar, session_id varchar, location varchar, user_agent varchar);""")
+songplay_table_create = ("""create table if not exists songplays(songplay_id serial primary key, start_time timestamp references time(start_time), \
+user_id varchar references users(user_id), level varchar, song_id varchar references songs(song_id), artist_id varchar references artists(artist_id), \
+session_id varchar, location varchar, user_agent varchar);""")
 
-user_table_create = ("""create table if not exists users(user_id varchar, first_name varchar, last_name varchar, \
+user_table_create = ("""create table if not exists users(user_id varchar primary key, first_name varchar, last_name varchar, \
 gender varchar, level varchar);""")
 
-song_table_create = ("""create table if not exists songs(song_id varchar, title varchar, artist_id varchar, year int, \
+song_table_create = ("""create table if not exists songs(song_id varchar primary key, title varchar, artist_id varchar, year int, \
 duration float);""")
 
-artist_table_create = ("""create table if not exists artists(artist_id varchar, name varchar, location varchar, \
+artist_table_create = ("""create table if not exists artists(artist_id varchar primary key, name varchar, location varchar, \
 latitude varchar, longitude varchar);""")
 
-time_table_create = ("""create table if not exists time(start_time timestamp, hour int, day int, week int, month int, \
+time_table_create = ("""create table if not exists time(start_time timestamp primary key, hour int, day int, week int, month int, \
 year int, weekday int);""")
 
 # INSERT RECORDS
@@ -51,5 +52,5 @@ limit 1
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [user_table_drop, song_table_drop, artist_table_drop, time_table_drop, songplay_table_drop]
